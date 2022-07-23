2a02327ba6d224c9b2e3ca4531360bf75ad007db
OrantFromBenedictusTeam <domitrz.piotr2@gmail.com>
2022-07-23 14:32:58 +0200

    repository interface corrections

--- src/MovieLibrary.Api/Common/IRepository.cs
+++ src/MovieLibrary.Api/Common/IRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace MovieLibrary.Api.Common
+﻿using Microsoft.EntityFrameworkCore.Query;
+using MovieLibrary.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieLibrary.Api.Common
 {
-    public class IRepository
+    public interface IRepository<TEntity> where TEntity : Entity
     {
+        int Insert(TEntity entity);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> GetAll(Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> config);
+        TEntity GetById(int id);
+        void Update(TEntity entity);
+        void Delete(int id);
     }
 }
