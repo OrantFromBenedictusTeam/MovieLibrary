0b0c480c515575a25bd5f6671185ad23a698a697
OrantFromBenedictusTeam <domitrz.piotr2@gmail.com>
2022-07-23 15:06:37 +0200

    Abstract crud controller was added and mediatR was installed

---
+++ src/MovieLibrary.Api/Controllers/CrudApiController.cs
@@ -0,0 +1,46 @@
+﻿
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MovieLibrary.Api.Common;
+using MovieLibrary.Data.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("/v1/[controller]")]
+    public abstract class CrudApiController<TEntity, TCreateDto, TUpdateDto, TGetDto> : ControllerBase where TEntity : Entity
+    {
+        protected IRepository<TEntity> Repository { get; }
+        protected IMapper Mapper { get; }
+        public CrudApiController(IRepository<TEntity> repository, IMapper mapper)
+        {
+            Repository = repository;
+            Mapper = mapper;
+        }
+
+        [HttpGet]
+        public virtual IEnumerable<TGetDto> Get()
+        {
+            return Repository.GetAll().Select(item => Mapper.Map<TGetDto>(item));
+        }
+
+        [HttpGet("{id}")]
+        public virtual TGetDto Get(int id) => Mapper.Map<TGetDto>(Repository.GetById(id));
+
+        [HttpPost]
+        public virtual int Create([FromBody] TCreateDto dto) => Repository.Insert(Mapper.Map<TEntity>(dto));
+
+        [HttpDelete("{id}")]
+        public virtual void Delete(int id) => Repository.Delete(id);
+
+        [HttpPut]
+        public virtual TGetDto Update([FromBody] TUpdateDto dto)
+        {
+            var updatingEntity = Mapper.Map<TEntity>(dto);
+            Repository.Update(updatingEntity);
+            return Mapper.Map<TGetDto>(Repository.GetById(updatingEntity.Id));
+        }
+    }
+}
